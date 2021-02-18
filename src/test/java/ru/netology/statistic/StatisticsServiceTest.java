da186c899b8b098828cec2daba3d79219fd41ae1
Natalya Berezovski <natalya.berezovski@gmail.com>
2021-02-18 15:27:49 +0200

    Commit

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,7 +10,7 @@ class StatisticsServiceTest {
   void findMax() {
     StatisticsService service = new StatisticsService();
 
-    long[] incomesInBillions = {13, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+    long[] incomesInBillions = {18, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
     long expected = 17;
 
     long actual = service.findMax(incomesInBillions);
